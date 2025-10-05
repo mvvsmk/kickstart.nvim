8993c1bd8ae8269fa1e13f2aa5f55ba87e55f1c3
M V V S Manoj Kumar <mvvsmanojkumar@gmail.com>
2025-10-05 13:18:05 +0530

    Added oil, mini_icons, and lazy git with a test of copy function

--- init.lua
+++ init.lua
@@ -114,7 +114,11 @@ vim.o.showmode = false
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
+  if vim.g.neovide then
+    vim.o.clipboard = 'clip'
+  else
+    vim.o.clipboard = 'unnamedplus'
+  end
 end)
 
 -- Enable break indent
@@ -973,11 +977,11 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
